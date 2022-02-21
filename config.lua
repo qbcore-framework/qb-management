7f0cd85ab15df7c1da967eb69aa7e0f994f4d960
AJ <66404074+ihyajb@users.noreply.github.com>
2022-02-21 05:14:47 -0500

    fix: Restore Default Config

--- config.lua
+++ config.lua
@@ -1,28 +1,12 @@
 Config = {}
 
 Config.Jobs = {
-    ['police'] = vector3(460.76, -985.58, 30.73),
-    ['ambulance'] = vector3(334.79, -594.39, 43.28),
-    ['realestate'] = vector3(-724.66, 264.25, 83.62),
-    ['cardealer'] = vector3(-25.51, -1104.64, 27.27),
-    ['mechanic'] = vector3(-339.53, -157.31, 44.59),
-    ['burgershot'] = vector3(-1177.65, -896.98, 13.97),
-    ['whitewidow'] = vector3(183.97, -251.95, 54.07),
-    ['usedcardealer'] = vector3(124.81, -3013.84, 7.04),
-    ['vanilla'] = vector3(95.36, -1293.61, 29.64),
-    ['pawnshop'] = vector3(167.43, -1313.38, 29.36),
-    ['government'] = vector3(167.43, -1313.38, 29.36),
-    ['henhouse'] = vector3(-294.67, 6266.69, 34.84),
-    ['catcafe'] = vector3(-596.42, -1051.58, 22.34),
-    ['pizza'] = vector3(798.7, -750.71, 31.27),
-    ['fire'] = vector3(194.13, -1644.07, 29.8),
-    ['tequilala'] = vector3(-561.27, 289.53, 82.18),
-    ['corretto'] = vector3(-1180.33, -1408.67, 10.52),
-    ['beanmachine'] = vector3(-631.41, 223.71, 81.88),
-    ['hayes'] = vector3(-1428.15, -458.53, 35.91),
-    ['galaxy'] = vector3(389.68, 272.64, 94.99),
-    ['triadrecords'] = vector3(-816.45, -698.79, 32.14),
-    ['arcade'] = vector3(-1649.52, -1070.1, 13.76),
+    ['police'] = vector3(448.4, -973.2, 30.6),
+    ['ambulance'] = vector3(310.1, -599.43, 43.29),
+    ['realestate'] = vector3(-124.786, -641.486, 168.820),
+    ['taxi'] = vector3(903.32, -170.55, 74.0),
+    ['cardealer'] = vector3(-32.0, -1114.2, 26.4),
+    ['mechanic'] = vector3(-347.41, -133.48, 39.01)
 }
 
 Config.Gangs = {
