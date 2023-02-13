ac3951e64daf7646f9d2aa5125c5584741383c90
Pride1922 <48358139+Pride1922@users.noreply.github.com>
2023-02-13 16:27:16 +0100

    Update pt.lua

--- locales/pt.lua
+++ locales/pt.lua
@@ -1,9 +1,9 @@
 local Translations = {
-    headers = {
-        ['bsm'] = 'Menu do Chefe - ',
-    },
-    body = {
-        ['manage'] = 'Gerir Funcionários',
+	headers = {
+		['bsm'] = 'Menu do Chefe - ',
+	},
+	body = {
+		['manage'] = 'Gerir Funcionários',
 		['managed'] = 'Verificar sua Lista de Funcionários',
 		['hire'] = 'Contratar Funcionários',
 		['hired'] = 'Contratar Civis próximos',
@@ -30,57 +30,57 @@ local Translations = {
 		['amount'] = 'Montante',
 		['return'] = 'Voltar',
 		['exit'] = 'Voltar',
-    },
-    drawtext = {
-        ['label'] = '[E] Abrir Gestão ',
-    },
-    target = {
-        ['label'] = 'Menu do Chefe',
-    },
-    headersgang = {
-        ['bsm'] = 'Gerir Gang  - ',
-    },
-    bodygang = {
-        ['manage'] = 'Gerir Membros do Gang',
-	    ['managed'] = 'Recrutar ou Despedir Membros do Gang',
-	    ['hire'] = 'Recrutar Membros',
-	    ['hired'] = 'Contratar Membros do Gang',
-	    ['storage'] = 'Acesso ao Armazenamento',
-	    ['storaged'] = 'Abrir Armazenamento do Gang',
-	    ['outfits'] = 'Roupas',
-	    ['outfitsd'] = 'Mudar de Roupa',
-	    ['money'] = 'Gestão de Dinheiro',
-	    ['moneyd'] = 'Verificar o Saldo do Gang',
-	    ['mempl'] = 'Gerir Membros do Gang - ',
-	    ['mngpl'] = 'Gerir ',
-    	['grade'] = 'Grau: ',
-	    ['fireemp'] = 'Despedir',
-	    ['hireemp'] = 'Contratar Membros - ',
-	    ['cid'] = 'ID de cidadão: ',
-        ['balance'] = 'Saldo: €',
-        ['deposit'] = 'Depositar',
-        ['depositd'] = 'Depositar dinheiro na conta',
-        ['withdraw'] = 'Levantamento',
-        ['withdrawd'] = 'Levantar dinheiro da conta',
-        ['depositm'] = 'Depositar Dinheiro <br> Saldo Disponível: €',
-        ['withdrawm'] = 'Levantar Dinheiro <br> Saldo Disponível: €',
-        ['submit'] = 'Confirmar',
-        ['amount'] = 'Montante',
-        ['return'] = 'Voltar',
-        ['exit'] = 'Voltar',
-    },
-    drawtextgang = {
-        ['label'] = '[E] Abrir Gestão Gang',
-    },
-    targetgang = {
-        ['label'] = 'Gang Menu',
-    }
+	},
+	drawtext = {
+		['label'] = '[E] Abrir Gestão ',
+	},
+	target = {
+		['label'] = 'Menu do Chefe',
+	},
+	headersgang = {
+		['bsm'] = 'Gerir Gang  - ',
+	},
+	bodygang = {
+		['manage'] = 'Gerir Membros do Gang',
+		['managed'] = 'Recrutar ou Despedir Membros do Gang',
+		['hire'] = 'Recrutar Membros',
+		['hired'] = 'Contratar Membros do Gang',
+		['storage'] = 'Acesso ao Armazenamento',
+		['storaged'] = 'Abrir Armazenamento do Gang',
+		['outfits'] = 'Roupas',
+		['outfitsd'] = 'Mudar de Roupa',
+		['money'] = 'Gestão de Dinheiro',
+		['moneyd'] = 'Verificar o Saldo do Gang',
+		['mempl'] = 'Gerir Membros do Gang - ',
+		['mngpl'] = 'Gerir ',
+		['grade'] = 'Grau: ',
+		['fireemp'] = 'Despedir',
+		['hireemp'] = 'Contratar Membros - ',
+		['cid'] = 'ID de cidadão: ',
+		['balance'] = 'Saldo: €',
+		['deposit'] = 'Depositar',
+		['depositd'] = 'Depositar dinheiro na conta',
+		['withdraw'] = 'Levantamento',
+		['withdrawd'] = 'Levantar dinheiro da conta',
+		['depositm'] = 'Depositar Dinheiro <br> Saldo Disponível: €',
+		['withdrawm'] = 'Levantar Dinheiro <br> Saldo Disponível: €',
+		['submit'] = 'Confirmar',
+		['amount'] = 'Montante',
+		['return'] = 'Voltar',
+		['exit'] = 'Voltar',
+	},
+	drawtextgang = {
+		['label'] = '[E] Abrir Gestão Gang',
+	},
+	targetgang = {
+		['label'] = 'Gang Menu',
+	}
 }
 
 if GetConvar('qb_locale', 'en') == 'pt' then
-    Lang = Locale:new({
-        phrases = Translations,
-        warnOnMissing = true,
-        fallbackLang = Lang,
-    })
+	Lang = Locale:new({
+		phrases = Translations,
+		warnOnMissing = true,
+		fallbackLang = Lang,
+	})
 end
