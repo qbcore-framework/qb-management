85dfa54ce0afb7bbb580993b9c63b0b399e26654
AnishBplayz <anishbplayz@gmail.com>
2023-11-19 23:17:29 +0530

    Update sv_gang.lua

--- server/sv_gang.lua
+++ server/sv_gang.lua
@@ -167,7 +167,7 @@ RegisterNetEvent('qb-gangmenu:server:FireMember', function(target)
 		if player[1] ~= nil then
 			Employee = player[1]
 			Employee.gang = json.decode(Employee.gang)
-			if Employee.gang.grade.level > Player.PlayerData.job.grade.level then TriggerClientEvent('QBCore:Notify', src, "You cannot fire this citizen!", "error") return end
+			if Employee.PlayerData.gang.grade.level > Player.PlayerData.gang.grade.level then TriggerClientEvent('QBCore:Notify', src, "You cannot fire this citizen!", "error") return end
 			local gang = {}
 			gang.name = "none"
 			gang.label = "No Affiliation"
